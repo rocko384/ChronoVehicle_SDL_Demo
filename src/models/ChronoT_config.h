0d81d88c1c92b967034ca562443064858374ca43
Charles Ricchio <charlie@ricchio.com>
2015-01-02 17:56:53 -0600

    First

---
+++ src/models/ChronoT_config.h
@@ -0,0 +1,12 @@
+// -----------------------------------------------------------------
+// ChronoT configuration header file
+// -----------------------------------------------------------------
+
+// Path to CHRONO data
+#define CHRONO_DATA_DIR "C:/Users/AdamsCar/Documents/repositories/chrono/src/demos/data/"
+
+// Specify if Irrlicht support is available
+#define IRRLICHT_ENABLED 1
+
+// Specify if IrrKlang support is enabled
+#define IRRKLANG_ENABLED 1
